488819a4d19a1767a6d714dbf4e4ae4e425ebd32
frederico-silva <frederico.aos@gmail.com>
2025-09-10 16:18:30 -0300

    exercicio 1 - maior que cinquenta

---
+++ exercicios/maiorQueCinquenta.m
@@ -0,0 +1,11 @@
+% This code verifies if a number is greater than 50
+% if num > 50 the output is the half of num
+
+num = input("Type a number: ");
+
+if (num > 50)
+    res = num/2;
+    fprintf("The number %.2f is %.2f \n", num, res);
+else
+   fprintf("The num %.2f is less than or equal 50 \n", num)
+end 
